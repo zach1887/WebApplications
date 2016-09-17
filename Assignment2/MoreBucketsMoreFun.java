c2b9a3288f5b958472df4310de9f74b5a1752381
Jesse Cunningham <Jesse Cunningham>
2016-09-17 01:15:47 +0100

    JECUNINDWRK -2 Files for the second assignment

---
+++ Assignment2/MoreBucketsMoreFun.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mavenproject2;
+
+/**
+ *
+public class MoreBucketsMoreFun {
+    public static void main(String[] args) {
+        // DECLARE ALL THINGS
+        // Always declare at the the beginning
+        int butterflies, beetles, bugs;
+        String color, size, shape, thing;
+        double number;
+        
+        // Initialize a few of them
+        butterflies = 2;
+        beetles = 4;
+        
+        bugs = butterflies + beetles;
+        System.out.println("There are only " + butterflies + " butterflies left.");
+        System.out.println("but " + bugs + " bugs total.");
+        
+        System.out.println("Uh oh, my dogs ate one");
+        butterflies--;
+        
+        System.out.println("Now there are only " + butterflies + " butterflies left.");
+        System.out.println("But still " + bugs + " bugs left, but wait a minute!!");
+        System.out.println("Maybe I just counted wrong the first time.");
+        
+    }
+}
---
+++ Assignment2/TheOrderOfThings.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mavenproject2;
+
+/**
+ *
+
+public class TheOrderOfThings {
+    public static void main(String[] args) {
+        int number;
+        String opinion, size, age, shape, color, origin, material, purpose;
+        String noun;
+        
+        number = 4;
+        opinion = "AVERAGE";
+        size = "small";
+        age = "older";
+        shape = "oblong";
+        color ="cyan";
+        origin="Eastern Asia";
+        material = "silver";
+        purpose = "neutral good";
+        
+        noun = "dragons";
+        
+        //Using the + with strings to concatenate instead of adding
+        System.out.println(number  + " " + size +  " " + age + " " 
+                + opinion + " " + color  + " " + origin + " " 
+                + material + " " + purpose +   " " + noun + ".");
+        
+        System.out.println(number + " " + size  + " " + age +  " " + opinion 
+                + " " + purpose + " " + color  + " " + origin   + " " + material 
+                + " " + noun + ".");
+        
+        
+    }
+    
+}
