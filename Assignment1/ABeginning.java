5aebdb6b932da1d0e4a602414bc12817da8c9ec8
Jesse Cunningham <Jesse Cunningham>
2016-09-17 01:14:16 +0100

    JECUINDWRK-1 Files for Assignment 1

---
+++ Assignment1/ABeginning.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ */
+public class ABeginning {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello Guild!");
+        System.out.println("Coding is easier than I thought.");
+        System.out.println("No, I'm not writing that.");
+        System.out.println("After I finish typing");
+        System.out.println("I'll compile my code");
+        System.out.println("And then I will run it");
+        System.out.println("The consoloe will print out all of my words.");
+        System.out.println("And it all starts with \"Hello World!\"..");
+    }
+    
+}
