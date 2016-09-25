45aa6e1244070a8a1599ce60fc46ff5c0b961eb2
Jesse Cunningham <zach92indy@gmail.com>
2016-09-25 05:56:20 +0100

    JECUINDWRK-9 State Capital assignments

---
+++ mavenproject1/src/main/java/Section2Lab3/CapitalInfo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Section2Lab3;
+
+/**
+ *
+ * @author Jesse
+ */
+public class CapitalInfo {
+    public String capitalName;
+    public int populationSize;
+    public double capitalArea;
+
+    public CapitalInfo(String capitalName, int populationSize, double capitalArea) {
+        this.capitalName = capitalName;
+        this.populationSize = populationSize;
+        this.capitalArea = capitalArea;
+    }
+    
+    
+    
+}
